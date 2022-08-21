e7f13b289ef3c19615ed19e7fdeb8f1df3bdb15c
James Njenga <knjenga94@gmail.com>
2022-08-21 23:09:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+
+var customerName="bob";
+
+function upperCaseCustomerName() {
+    return customerName= customerName.toUpperCase();
+}
+function setBestCustomer(){
+    bestCustomer='not bob'
+}
+function overwriteBestCustomer(newOne){
+    bestCustomer=newOne
+}
+const leastFavoriteCustomer="Maybe bob"
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer="Maybe bob"
+}
+
+/*function upperCaseCustomerName(){
+    return (customerName.toUpperCase());
+}
+const ans=upperCaseCustomerName();
+
+console.log(ans);
+
+function setBestCustomer(){
+    var bestCustomer="not bob";
+}
+setBestCustomer();
+
+//cfunction setBestCustomer()*/
